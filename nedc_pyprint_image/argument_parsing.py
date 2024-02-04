36c12aad7be35895b2ae8814bce790cec0f2e3db
Leo-Berman <leo.berman@temple.edu>
2024-02-04 13:22:10 -0500

    added parsing file

---
+++ nedc_pyprint_image/argument_parsing.py
@@ -0,0 +1,18 @@
+'''Make a parsing system using the help function form the C program
+and use this directory /data/isip/tools/linux_x64/nfc/util/python/nedc_pyprint_signal 
+as a framework on how to use their tools for our argument parsing. All we're doing is
+implimenting their argument parsing tool.'''
+
+# parse arguments in this function
+def parse_args():
+    
+    # parse arguments
+
+
+
+# test function from here
+def main():
+
+    # print parsed arguments from here 
+
+main()

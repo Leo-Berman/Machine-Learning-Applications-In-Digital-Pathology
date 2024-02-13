fcae197d2e7a921cc37d1fa4cacd4b02c959d0b8
Leo-Berman <leo.berman@temple.edu>
2024-02-13 11:09:20 -0500

    worked on implimenting argument parsing and using window to jpg and added to pycache

--- nedc_pyprint_image/nedc_pyprint_image.py
+++ nedc_pyprint_image/nedc_pyprint_image.py
@@ -3,39 +3,13 @@ import sys
 import argparse as agp
 import numpy as np
 from PIL import Image
-
+import argument_parsing as argspy
+import nedc_printwindows as winprint 
 # add 
 sys.path.insert(0,"/data/isip/tools/linux_x64/nfc/class/python/nedc_image_tools/nedc_image_tools.py")
-def argument_parsing():
-    parser = agp.ArgumentParser(
-        prog = 'nedc_pypring_image.py',
-        description = 'prints the image values for an SVS file'
-    )
 
-    parser.add_argument('--height',default=10)
-    parser.add_argument('--width',default=10)
-    parser.add_argument('-l','--level',default=0)
-    parser.add_argument('-x','--xoff',default=0)
-    parser.add_argument('-y','--yoff',default=0)
-    parser.add_argument('-f','--filename',default="/data/isip/data/fccc_dpath/deidentified/v1.0.0/svs/00000/000000197/001003366/c50.2_c50.2/000000197_001003366_st065_xt1_t000.svs")
-    args = parser.parse_args()
-    return args
 
-def windows_to_jpg(nil,size):
-    # Get dimensions
-    xdim,ydim =NIL.get_dimension()
-    print("Dimensions = ",xdim,ydim)
-    
-    # Get all the coordinates for each windows
-    coordinates = [(x, y) for x in range(0, xdim, WINDOW_FRAME) for y in range(0, ydim, WINDOW_FRAME)]
-    
-    # Read all the windows for each coordinate WINDOW_FRAME x WINDOW_FRAME
-    windows = NIL.read_data_multithread(coordinates, WINDOW_FRAME, WINDOW_FRAME)
-    
-    # save all the images as JPEGS
-    for index, window in enumerate(windows):
-        im = Image.fromarray(window)
-        im.save(f'./DATA/window_{index}.jpg', "JPEG")
+
 
 
 
@@ -66,16 +40,25 @@ def main():
     # print useful processing message
     print("beginning argument processing...")
 
+    args = argspy.parse_args()
+    
+
     # get parses as variables
-    fname = "/data/isip/data/fccc_dpath/deidentified/v1.0.0/svs/00000/000000197/001003366/c50.2_c50.2/000000197_001003366_st065_xt1_t000.svs"
-    height = 10
-    width = 10
-    level = 0
-    xoff = 0
-    yoff = 0
+    fname =  args.filename#"/data/isip/data/fccc_dpath/deidentified/v1.0.0/svs/00000/000000197/001003366/c50.2_c50.2/000000197_001003366_st065_xt1_t000.svs"
+    height = args.height#10
+    width =  args.width#10
+    level =  args.level#0
+    xoff =   args.xoff#0
+    yoff =   args.yoff#0
+
+    print("filename = ",fname,"height = ",height,"width = ",width,"level = ",level,"xoff = ",xoff,"yoff = ",yoff)
 
     # Class for using tools from Phuykong's library
     NIL = phg.Nil()
+
+    NIL2 = phg.Nil()
+    NIL2.open(fname)
+    winprint.windows_to_jpg(NIL2)
 
     # track how many need to be processed and how many need to be processed
     processed = 0

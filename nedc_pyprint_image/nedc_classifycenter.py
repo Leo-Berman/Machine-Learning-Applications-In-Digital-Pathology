2085d466f6af554bf38e524651aae01323e74b6e
yuan-ie <angela.nghiem1@gmail.com>
2024-02-21 00:38:06 -0500

    label parsing done

--- nedc_pyprint_image/nedc_classifycenter.py
+++ nedc_pyprint_image/nedc_classifycenter.py
@@ -5,8 +5,8 @@ import csv
 import re
 
 sys.path.insert(0,"/data/isip/tools/linux_x64/nfc/class/python/nedc_image_tools/nedc_image_tools.py")
-# labelfile = "/data/isip/data/tuh_dpath_breast/deidentified/v3.0.0/svs/train/00466205_aaaaaage/s000_2015_03_01/breast/00466205_aaaaaage_s000_0hne_0000_a001_lvl002_t000.csv"
-labelfile = "/home/tul16619/SD1/Machine-Learning-Applications-In-Digital-Pathology/nedc_pyprint_image/random_test.csv"
+labelfile = "/data/isip/data/tuh_dpath_breast/deidentified/v3.0.0/svs/train/00466205_aaaaaage/s000_2015_03_01/breast/00466205_aaaaaage_s000_0hne_0000_a001_lvl002_t000.csv"
+#labelfile = "/home/tul16619/SD1/Machine-Learning-Applications-In-Digital-Pathology/nedc_pyprint_image/random_test.csv"
 imgfile =0
 
 def parse_labels(file):
@@ -17,37 +17,47 @@ def parse_labels(file):
     '''
 
     # initialize empty lists
-    image_size = []
-    labels = []
-    
+    image_size = []     # holds the width (idx=0) and height (idx=1) in pixels
+    index = []          # empty list for all index values
+    region_id = []      # empty list for all regiod id values
+    tissue = []         # empty list for all tissue type
+    label = []          # empty list for all labels of regions
+    coord_index = []    # empty list for all coodinate index values
+    row = []            # empty list for all row values
+    column = []         # empty list for all column values
+    depth = []          # empty list for all depth values
+    confidence = []     # empty list for all confidence values
+    headers = [index, region_id, tissue, label, coord_index, row, column, depth, confidence]
 
     # open the CSV file in read mode
     with open(labelfile, 'r', newline='') as lfile:
         csvreader = csv.reader(lfile)
 
-        # extract labels and data
-        for row_index,row in enumerate(csvreader):
+        # extract headers and data
+        for row_index,r in enumerate(csvreader):
+            # extract image size data
             if row_index == 3:
-                for cell in row:
+                for cell in r:
                     pixel = re.findall('\d+', str(cell))
                     image_size.append(int(pixel[0]))
-                print(image_size)
-            # elif row_index == 6:
-            #     for cell in row:
-            #         label = str(cell)
-            #         labels.append(label)
-            #     print(labels)
-            #     # associate the labels to variables
-            #     index = labels[0]
-
-                
-        # # extract the image size
-        # for _ in range(3):
-        #     next(line)
-        # pixels = re.findall('\d+',line)
-        # print(pixels)
-
+            # extract data and store into corresonding lists
+            elif row_index >= 7:
+                for cell_index,cell in enumerate(r):
+                    value = str(cell)
+                    headers[cell_index].append(value)
     lfile.close()
+
+    # convert each string-list to the correct type except for tissue and label
+    index = list(map(int,index))
+    region_id = list(map(int,region_id))
+    coord_index = list(map(int,coord_index))
+    row = list(map(int,row))
+    column = list(map(int,column))
+    depth = list(map(int,depth))
+    confidence = list(map(float,confidence))
+
+    width = image_size[0]
+    height  = image_size[1]
 
 def classify_center(imgfile,labelfile,framesize = -1):
 

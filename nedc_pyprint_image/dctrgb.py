b4868b7666b74de482670c1d80f3a5cf77c851a9
Leo-Berman <leo.berman@temple.edu>
2024-02-22 12:57:12 -0500

    Started working on the discrete cosine transform
    I decided that I would put all the reds in a vector all the greens in a vector and all the blues in a vector. Then I would take the dct of each vector and concatenate all the vectors. I'm honestly not sure how to impliment this in a pca but I will look further into it.

--- nedc_pyprint_image/dctrgb.py
+++ nedc_pyprint_image/dctrgb.py
@@ -1,4 +1,30 @@
+import csv
+from scipy.fftpack import dct
 relpath = "./DATA/dcttest.csv"
 def main():
     '''do a discrete cosine transform of the data in relpath'''
+    file = open(relpath,'r')
+    data = csv.reader(file)
+    read = []
+    for row in data:
+        read.append(row)
+
+    r = []
+    g = []
+    b = []
+    a = []
+    for i in range(1,len(read[1])-1,4):
+        r.append(int(read[1][i]))
+        g.append(int(read[1][i+1]))
+        b.append(int(read[1][i+2]))
+        a.append(int(read[1][i+3]))
+    
+    vector = []
+    vector.extend(dct(r))
+    vector.extend(dct(g))
+    vector.extend(dct(b))
+    print(vector)
+    file = open("DATA/PCA.csv",'w')
+    writer = csv.writer(file)
+    writer.writerow(vector)
 main()

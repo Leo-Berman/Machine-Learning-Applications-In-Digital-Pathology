f9392b50412d48355ed02d866bbb82735e83b46d
Leo-Berman <leo.berman@temple.edu>
2024-02-24 22:19:42 -0500

    Adding comments to the code for printing out RGBA values

--- nedc_pyprint_image/printframevalues.py
+++ nedc_pyprint_image/printframevalues.py
@@ -1,28 +1,25 @@
-from PIL import Image
+# Import phuykongs library
 import nedc_image_tools as phg
 import sys
 sys.path.insert(0,"/data/isip/tools/linux_x64/nfc/class/python/nedc_image_tools/nedc_image_tools.py")
 
-def windows_to_jpg(imagefile,coord = (0,0), window_frame = [50,50]):
+# convert a single window to a list of rgb values
+# window will be a list of list of lists
+# the first layer of list will encompass the whole window
+# each underlaying list will be a row
+# each underlaying list of the row will be a pixel
+# the label is the label Yuan will feed the code
+def window_to_rgb(imagefile,label,coord = (0,0), window_frame = [50,50]):
 
     # open the imagefile
     NIL = phg.Nil()
     NIL.open(imagefile)
 
-    # Get dimensions
-    # xdim,ydim =NIL.get_dimension()
-
-    #
-    # if window_frame == [-1,-1]:
-        # window_frame = [xdim,ydim]
-
-    # Get all the coordinates for each windows
-    # coordinates = [(x, y) for x in range(0, xdim, window_frame[0]) for y in range(0, ydim, window_frame[1])]
-    # print(coordinates)
-    # Read all the windows for each coordinate WINDOW_FRAME x WINDOW_FRAME
-    windows = NIL.read_data_multithread([coord],window_frame[0],window_frame[1])
+    # read the single frame
+    window = NIL.read_data_multithread([coord],window_frame[0],window_frame[1])
     
     # save all the images as JPEGS
-    print(windows)
+    print(window)
 
-windows_to_jpg("/data/isip/data/tuh_dpath_breast/deidentified/v2.0.0/svs/train/00707578/s000_2015_04_01/breast/00707578_s000_0hne_0000_a001_lvl000_t000.svs",(0,0),[50,50])
+# call the function
+window_to_rgb("/data/isip/data/tuh_dpath_breast/deidentified/v2.0.0/svs/train/00707578/s000_2015_04_01/breast/00707578_s000_0hne_0000_a001_lvl000_t000.svs",1,(0,0),[50,50])

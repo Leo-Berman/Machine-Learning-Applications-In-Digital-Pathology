4c64400302922a5ec6bf108bd017d50a36e456bc
Leo-Berman <leo.berman@temple.edu>
2024-02-24 22:24:11 -0500

    comments for muho's tasks

--- nedc_pyprint_image/printframevalues.py
+++ nedc_pyprint_image/printframevalues.py
@@ -9,6 +9,10 @@ sys.path.insert(0,"/data/isip/tools/linux_x64/nfc/class/python/nedc_image_tools/
 # each underlaying list will be a row
 # each underlaying list of the row will be a pixel
 # the label is the label Yuan will feed the code
+# This function is going to take a frame and a label and write that frame
+# and label to a csv file. That csv file is going to be in the format
+# of relativepath (./DATA/dcttest.csv)
+#
 def window_to_rgb(imagefile,label,coord = (0,0), window_frame = [50,50]):
 
     # open the imagefile

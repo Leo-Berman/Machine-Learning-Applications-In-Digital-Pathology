1305e0734fa1670105b6edb4bd436c2e7ca51f43
Muhotasim Ahmed <muho@temple.edu>
2024-03-15 11:41:03 -0400

    Deleted more cmnts

--- nedc_pyprint_image/printframevalues.py
+++ nedc_pyprint_image/printframevalues.py
@@ -2,6 +2,9 @@
 import nedc_image_tools as phg
 import sys
 import csv
+import numpy as np
+from scipy.fftpack import dct, idct
+import matplotlib.pyplot as plt
 
 sys.path.insert(0,"/data/isip/tools/linux_x64/nfc/class/python/nedc_image_tools/nedc_image_tools.py")
 
@@ -35,3 +38,101 @@ def window_to_rgb(imagefile,labels,coords = [(0,0)], window_frame = [50,50],name
 
     return window_list
 
+
+
+def rgba_to_dct(framevalues):
+    # path = 'DATA/dctoutput.csv'
+
+    # Open CSV file in read mode with newline to skip "/n"
+    # Create a list from the CSV reader object
+
+
+
+
+    # Create individual lists for each value of RGBA
+    #
+
+    red = []
+    green = []
+    blue = []
+    alpha = []
+
+    # Append corresponding list values in separate RGBA lists
+    #
+
+    for i in range(1,len(framevalues)-1,4):
+        red.append(framevalues[i])
+        green.append(framevalues[i+1])
+        blue.append(framevalues[i+2])
+        alpha.append(framevalues[i+3])
+
+  
+    #Display the RGBA lists
+    #
+        
+    #print(f"Red: {red}")
+    #print(f"Green: {green}")
+    #print(f"Blue: {blue}")
+    #print(f"Alpha: {alpha}")
+    
+    # concatenate the dcts of each vector
+    # probably need to index each DCT for the most
+    # signifcant terms
+    #
+    
+    vector = []
+    vector.extend(dct(red)[0:10])
+    vector.extend(dct(green)[0:10])
+    vector.extend(dct(blue)[0:10])
+    vector.extend(dct(alpha)[0:10])
+
+    #print(len(vector))
+
+    # Convert vector to numpy array
+    vector_numpy = np.array(vector).tolist()
+
+    # Get absolute values of coefficients
+    #
+
+    # magnitude_coeffs = np.abs(vector)
+    #print(magnitude_coeffs)
+
+    # Determine a high pass threshold
+    # 75th percentile
+    #
+
+    # threshold = np.percentile(magnitude_coeffs, 99.999)
+    # print(threshold)
+
+
+    # Performing an Inverse DCT to reconstruct original vector
+    # Many of the transformed DCT coefficients are close to zero
+    # After inverse DCT those coefficients are automatically getting discarded
+    
+    # reconstructed_vector = idct(vector)
+
+    # print(type(reconstructed_vector))
+
+    # High Pass Filter
+    # Preserving Higher Frequencies (Lower Magnitudes/ Energies)
+    # As Higher Frequencies denote texture boundaries
+
+    # High_Freq_Coeffs = vector_numpy[magnitude_coeffs < threshold]
+    # High_Freq_Coeffs = High_Freq_Coeffs.tolist()
+    # print(High_Freq_Coeffs)
+    vector_numpy.insert(0,framevalues[0])
+
+
+
+    file = open("DATA/QDATrain1.csv",'a')
+    writer = csv.writer(file)
+    writer.writerow(vector_numpy)
+
+# if __name__ == "__main__":
+    # main()
+
+
+
+
+
+    

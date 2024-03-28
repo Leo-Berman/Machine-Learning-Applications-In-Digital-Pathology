c4bfd5a0306c38b84a08ecbbeec9e616c9613869
Leo-Berman <leo.berman@temple.edu>
2024-03-28 10:33:00 -0400

    more work on refactoring

--- nedc_mladp/lib/nedc_fileio.py
+++ nedc_mladp/lib/nedc_fileio.py
@@ -34,101 +34,6 @@ def getargs():
     # return the file
     return(parameter_file)
 
-def parameter_search(parameter, parameter_file):
-    """
-        :Objective:
-        This function searches for the specific parameter through the parameter file.
-
-        :param parameter: The specified parameter.
-        :type parameter: str
-
-        :param parameter_file: csv file containing parameters.
-        :type parameter_file: str (.csv format)
-
-        :return: Returns the value of the specific parameter or 0 if None.
-        :rtype: int, float, or string.
-    """
-
-    # read the parameter file
-    with open(parameter_file, mode='r') as csv_file:
-        csv_reader = csv.reader(csv_file)
-        # look for parameter
-        for row in csv_reader:
-            if row[0].startswith(parameter):
-                # if there is no value associated with the parameter, set to default value
-                if row[1] is None:
-                    return default_value(parameter)
-                # if there is a value associated with the parameter, return the value
-                else:
-                    return row[1]
-
-def default_value(parameter):
-    """
-        :Description:
-        Default values of parameters if parameter value is None.
-
-        :param parameter: The specified parameter.
-        :type parameter: str
-    """
-
-    DEF_FRAMESIZE = int(-1)
-    DEF_WINDOWSIZE = int(-1)
-    
-    if parameter == "framesize":
-        value = DEF_FRAMESIZE
-    elif parameter == "windowsize":
-        value = DEF_WINDOWSIZE
-    
-    # elif parameter == "level":
-    #     value = DEF_LVL
-    
-    # elif parameter == "xoff":
-    #     value = DEF_XOFF
-    
-    # elif parameter == "yoff":
-    #     value = DEF_YOFF
-
-def parse_parameters(parameter_file:str):
-    """
-        :Objective:
-            This function parses through the parameter file which is a csv file containing different parameters with associated values.
-            The parameter file contains the framesize (frame size), windowsize (window size), level, xoff (x-offset), yoff (y-offset), imagefile_list (text file of .svs files), and labelfile_list (text file of .csv files).
-    
-        :param parameter_file: csv file containing parameters.
-        :type parameter_file: str (.csv format)
-                
-        :return: Returns the values of the parsed parameters.
-        :rtype: text files, ints, and floats
-    """
-    # define default argument values
-    
-    # DEF_LVL = int(0)
-    # DEF_XOFF = float(0)
-    # DEF_YOFF = float(0)
-
-    param1 = "framesize"
-    framesize = parameter_search(param1, parameter_file)
-
-    param2 = "windowsize"
-    windowsize = parameter_search(param2, parameter_file)
-
-    # param3 = "level"
-    # level = parameter_search(param3, parameter_file)
-
-    # param4 = "xoff"
-    # xoff = parameter_search(param4, parameter_file)
-
-    # param5 = "yoff"
-    # yoff = parameter_search(param5, parameter_file)
-
-    param6 = "imagefile_list"
-    imagefile_list = parameter_search(param6, parameter_file)
-
-    param7 = "labelfile_list"
-    labelfile_list = parameter_search(param7, parameter_file)
-
-    return (imagefile_list, labelfile_list, int(windowsize), int(framesize))
-
 def read_list_of_files(csv_file:str):
     return(pandas.read_csv(csv_file).to_list())
 
@@ -271,7 +176,7 @@ def RGBA_to_dct(framelist,imagename):
         list_of_rows.append(vector_numpy)
 
     ifile,iextension = os.path.splitext(os.path.basename(os.path.normpath(imagename)))
-    file = open("../dat/EVAL_DATA2/"+ifile+"_RGBAVALUES.csv",'w')
+    file = open("../dat/DATA/"+ifile+"_RGBAVALUES.csv",'w')
     writer = csv.writer(file)
     for x in list_of_rows:
         writer.writerow(x)
--- nedc_mladp/src/gen_feats.py
+++ nedc_mladp/src/gen_feats.py
@@ -1,20 +1,28 @@
 import sys
 sys.path.append("../lib")
-
+sys.path.append("/data/isip/tools/linux_x64/nfc/class/python/nedc_sys_tools")
 import pandas
-import nedc_fileio
 import nedc_regionid
+import nedc_file_tools
+import nedc_cmdl_parser
+
 
 def read_file_lists(file_name):
     df = pandas.read_csv(file_name)
     return df.iloc[:, 0].to_list()
 
 def main():
-    svs_list_file, csv_list_file, window_size, frame_size = nedc_fileio.parse_parameters("parameters.csv")
-    svs_list = read_file_lists(svs_list_file)
-    csv_list = read_file_lists(csv_list_file)
-    for svs,csv in zip(svs_list,csv_list):
-        nedc_regionid.classify_frames(svs,csv,window_size,frame_size)
+    args_usage = "gen_feats_usage.txt"
+    args_help = "gen_feats_help.txt"
+    argparser = nedc_cmdl_parser.Cmdl(args_usage,args_help)
+    argparser.add_argument('-p', type = str)
+    parsed_args = argparser.parse_args()
+    print(print(parsed_args))
+    # parsed_parameters = nedc_file_tools.load_parameters("picone_params_try.txt","gen_feats")
+    # svs_list = read_file_lists(parsed_parameters['imagefile_list'])
+    # csv_list = read_file_lists(parsed_parameters['labelfile_list'])
+    # for svs,csv in zip(svs_list,csv_list):
+    #     nedc_regionid.classify_frames(svs,csv,parsed_parameters['windowsize'],parsed_parameters['framesize'])
 
 
 if __name__ == "__main__":

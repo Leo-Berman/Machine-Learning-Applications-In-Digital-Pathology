399e8def1cff5d2cadf91974f1c1ab4adece4cd8
Leo-Berman <leo.berman@temple.edu>
2024-09-23 11:22:17 -0400

    example code

--- nedc_mladp/src/functs/nedc_mladp_geometry_tools/nedc_mladp_geometry_tools.py
+++ nedc_mladp/src/functs/nedc_mladp_geometry_tools/nedc_mladp_geometry_tools.py
@@ -2,24 +2,45 @@
 #
 import shapely
 import sys
+import matplotlib.pyplot
 
-# Picone's libraries
+# import NEDC libraries
 #
 import nedc_image_tools
 
-# generate a shape from border coordinates
-#
-def generatePolygon(coords:list)->shapely.Polygon:
-    shape = shapely.Polygon(coords)
+'''
+generate a shapely.Polygon from list of coordinates
+
+Parameters:
+    coordinates % list of tuples of coordinates
+Return:
+    shape % resulting shapely.Polygon
+'''
+def generatePolygon(coordinates:list)->shapely.Polygon:
+    shape = shapely.Polygon(coordinates)
     return shape
 
-# return the border of a shape
-#
-def getBorder(shape:shapley.Polygon)->list:
-    return (shape.exterior.xy)
+'''
+return the border of a shape
 
-# return the top left corner of every frame
-#
+Parameters:
+   shape % 2 dimensional shapely.Polygon
+Return:
+   border % tuple of arrays, index 0 is x coordinates,
+            and index 1 is y coordinates
+'''
+def getBorder(shape:shapely.Polygon)->tuple:
+    border = shape.exterior.xy
+    return border
+'''
+    return the top left corner of every frame
+Parameters:
+    imagefile % path to svs file
+    framesize % size of frame that will be used to segment image
+Return:
+    frame_top_left_coordiantes % list of tuples (x,y) containing
+                                 the top left corner of each frame
+'''
 def getFrameCoordinates(imagefile:str,framesize:int)->list:
     
     # open the imagefile
@@ -40,26 +61,20 @@ def getFrameCoordinates(imagefile:str,framesize:int)->list:
     return frame_top_left_coordinates
 
 
-
-# generate a list of shapes that are each uniform boxes
-# coords = list of list (return of get frame start)
-# frame = [framewidth,frameheight] or (framewidth,frameheight) IE. can be list or tuple
-# if frame is different from what you gave getframestart, your boxes will overlay each other
-#
 '''
-Ex:
-filepath = "path to svs file that is 100 pixesl by 100 pixels"
-framesize = [10,10]
-coords = getframestart(filepath,framesize)
-squares = createboxshapes(coords,framesize)
+turn all of the top left coordinates into a list of shapely.Polygons 
 
-square will be a list of polygons from the shapely library
+Parameters:
+    coordinates % list of top left corners
+    frame_size % size of the boxes length and width
+Return:
+    frames %  list of shapely.Polygon each containing a frame
 '''
-def createBoxes(coordinates:list,frame_size:int)->list:
+def createFrames(coordinates:list,frame_size:int)->list:
 
     # create a list for the shapely polygons
     #
-    boxes = []
+    frames = []
 
     # iterate through all the coordinates
     #
@@ -75,23 +90,42 @@ def createBoxes(coordinates:list,frame_size:int)->list:
 
         # append the polygon square to the list
         #
-        boxes.append(generate_polygon(shape_coordinates))
+        frames.append(generatePolygon(shape_coordinates))
 
     # return the list of shapley polygons
     #
-    return boxes
+    return frames
 
+
+'''
+Example driver function to show how this code might
+be used
+'''
 def main():
-    annotations = "/data/isip/data/tuh_dpath_breast/deidentified/v3.0.0/svs/train/00477780_aaaaaagg/s000_2017/breast/00477780_aaaaaagg_s000_0hne_0000_b003_lvl001_t000.xml"
+    # declare an image file to use
+    #
     image = "/data/isip/data/tuh_dpath_breast/deidentified/v3.0.0/svs/train/00477780_aaaaaagg/s000_2017/breast/00477780_aaaaaagg_s000_0hne_0000_a005_lvl001_t000.svs"
 
-    print("Hello")
-    
-    frame_coordinates = getFrameCoordinates(image,1000)
-    print(frame_coordinates)
+    # declare a frame size
+    #
+    frame_size = 5000
+
+    # obtain the top left coordinates of each frame
+    #
+    frame_coordinates = getFrameCoordinates(image,frame_size)
+
+    # generate the frames
+    #
+    frames = createFrames(frame_coordinates,frame_size)
+
+    # for each of the frames print the corresponding
+    # x and y coordinates
+    #
+    for i,frame in enumerate(frames):
+        print("Frame #" + str(i) + ":",
+              "\nX coordinates = ", getBorder(frame)[0],
+              "\nY coordinates = ", getBorder(frame)[1])
 
 if __name__ == "__main__":
-    print("Hello")
     main()
 
-print("Hello")

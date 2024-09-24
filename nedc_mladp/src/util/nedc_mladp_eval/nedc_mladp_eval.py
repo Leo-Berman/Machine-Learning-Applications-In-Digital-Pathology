4147e0e8042224dc1375f7c55e948ba91fba8525
Leo <leograntberman@gmail.com>
2024-09-24 11:53:27 -0400

    better formatting

--- nedc_mladp/src/util/nedc_mladp_eval/nedc_mladp_eval.py
+++ nedc_mladp/src/util/nedc_mladp_eval/nedc_mladp_eval.py
@@ -26,13 +26,10 @@ def main():
     parsed_parameters = nedc_file_tools.load_parameters(parameter_file,"eval_model")
     feature_data_list=parsed_parameters['data_list']
     model_path=parsed_parameters['model']
-    
-
     run_params = nedc_file_tools.load_parameters(parameter_file,"gen_feats")
     if run_params['run']==1:
         feature_data_list=run_params['output_list']
-        model_path = nedc_file_tools.load_parameters(parameter_file,"train_model")['model_output_path']
-    
+        model_path = nedc_file_tools.load_parameters(parameter_file,"train_model")['model_output_path']    
     generate_decisions=int(parsed_parameters['decisions'])
     decisions_path=parsed_parameters['output_decisions_path']
     if not (decisions_path.endswith('/')):
@@ -46,13 +43,21 @@ def main():
     # load the model
     #
     model = joblib.load(model_path)        
-    
+
+    # read the features files list
+    #
     feature_files_list = fileio_tools.read_file_lists(feature_data_list)
 
+    # read all the files data and the header
+    #
     filesdata,headers = fileio_tools.read_feature_files(feature_files_list, get_header = True)
 
+    # iterate through each file
+    #
     for data,currfile,header in zip(filesdata,feature_files_list,headers):
-        
+
+        # extract the data, labels, frame locations, and sizes
+        #
         mydata = data[:, 4::]
         labels = data[:,0].tolist()
         frame_locations = data[:,1:3].tolist()
@@ -73,18 +78,26 @@ def main():
         file_decision_path = decisions_path+currfile.split('/')[-1][:-11]+"DECISIONS.csv"
         print(file_decision_path)
 
+        # get the frame decisions
+        #
         frame_decisions = eval_tools.generate_frame_decisions(model,mydata,file_decision_path,frame_locations,framesizes,header)
 
+        # get the sparse matrixes
+        #
         sparse_matrixes = ann_tools.coords_to_dict(frame_decisions)
 
+        # generate a tuple of framesizes
+        #
         framesize_fib = (int(framesizes[0]),int(framesizes[0]))
-        
+
+        # generate a heatmap of labels
+        #
         heatmap = ann_tools.heatmap(sparse_matrixes,framesize_fib)
 
-        ann_tools.generate_region_decisions(heatmap,framesize_fib[0])
-        
-        print(heatmap)
-        
+        # generate a regions
+        #
+        regions = ann_tools.generate_region_decisions(heatmap,framesize_fib[0])
+                
         #if generate_histogram == 1:
         #    eval_tools.plot_histogram(labels,histogram_output)
 

66bfb0b0e6d90dac24efcdc264c59b19fda52564
Leo <leograntberman@gmail.com>
2024-09-24 12:38:03 -0400

    updating format

--- nedc_mladp/src/util/nedc_mladp_eval/nedc_mladp_eval.py
+++ nedc_mladp/src/util/nedc_mladp_eval/nedc_mladp_eval.py
@@ -24,21 +24,41 @@ def main():
     # parse parameters
     #
     parsed_parameters = nedc_file_tools.load_parameters(parameter_file,"eval_model")
-    feature_data_list=parsed_parameters['data_list']
-    model_path=parsed_parameters['model']
-    run_params = nedc_file_tools.load_parameters(parameter_file,"gen_feats")
-    if run_params['run']==1:
+
+    # list of feature files
+    #
+    features_data_list = parsed_parameters['features_data_list']
+
+    # path to joblib model
+    #
+    model_path=parsed_parameters['model_path']
+
+    # boolean of whether or not to write frames
+    #
+    write_frame_decisions=int(parsed_parameters['write_frame_decisions'])
+
+    # path to where to write frame decisions
+    #
+    output_frame_decisions_directory=parsed_parameters['output_frame_decisions_directory']
+    if not (output_frame_decisions_path.endswith('/')):
+        output_frame_decisions_path = decisions_path + '/'
+        
+    # boolean of whether or not to write regions
+    #
+    write_region_decisions=int(parsed_parameters['write_region_decisions'])
+
+    # path to where to write region decisions
+    #
+    output_region_decisions_directory=parsed_parameters['output_region_decisions_directory']
+    if not (output_region_decisions_path.endswith('/')):
+        output_region_decisions_path = decisions_path + '/'
+        
+    # if run is set, update a couple paths to reflect
+    #
+    run_parameters = nedc_file_tools.load_parameters(parameter_file,"gen_feats")
+    if run_parameters['run'] == 1:
         feature_data_list=run_params['output_list']
-        model_path = nedc_file_tools.load_parameters(parameter_file,"train_model")['model_output_path']    
-    generate_decisions=int(parsed_parameters['decisions'])
-    decisions_path=parsed_parameters['output_decisions_path']
-    if not (decisions_path.endswith('/')):
-        decisions_path = decisions_path + '/'
-    generate_histogram = int(parsed_parameters['label_histogram'])                          
-    histogram_output=parsed_parameters['output_histogram_path']
-    even_data = int(parsed_parameters['even_data'])
-    confusion_matrix_path=parsed_parameters['output_confmat_path']
-    generate_confusion_matrix=int(parsed_parameters['confusion_matrix'])
+        model_path = nedc_file_tools.load_parameters(parameter_file,"train_model")['model_output_path']
 
     # load the model
     #
@@ -46,41 +66,30 @@ def main():
 
     # read the features files list
     #
-    feature_files_list = fileio_tools.read_file_lists(feature_data_list)
+    features_files_list = fileio_tools.read_file_lists(features_data_list)
 
     # read all the files data and the header
     #
-    filesdata,headers = fileio_tools.read_feature_files(feature_files_list, get_header = True)
+    files_data,headers = fileio_tools.read_feature_files(features_files_list, get_header = True)
 
     # iterate through each file
     #
-    for data,currfile,header in zip(filesdata,feature_files_list,headers):
+    for data,currfile,header in zip(files_data,feature_files_list,headers):
 
         # extract the data, labels, frame locations, and sizes
         #
-        mydata = data[:, 4::]
+        features = data[:, 4::]
         labels = data[:,0].tolist()
         frame_locations = data[:,1:3].tolist()
-        framesizes=data[:,3]
-        
-        # even the data out
-        #
-        if even_data == 1:
-            mydata,labels = feats_tools.even_data(mydata,labels)
+        frame_sizes=data[:,3]
             
-        # generate confusion matrix
-        #
-        #if generate_confusion_matrix == 1:
-        #    eval_tools.plot_confusion_matrix(model,labels,mydata,confusion_matrix_path)
-
         # generates a list of guess and their top level coordinates only applies to single image
         #
-        file_decision_path = decisions_path+currfile.split('/')[-1][:-11]+"DECISIONS.csv"
-        print(file_decision_path)
+        file_frame_decisions_path = decisions_path+currfile.split('/')[-1][:-11]+"DECISIONS.csv"
 
         # get the frame decisions
         #
-        frame_decisions = eval_tools.generate_frame_decisions(model,mydata,file_decision_path,frame_locations,framesizes,header)
+        frame_decisions = eval_tools.generate_frame_decisions(model,features,output_frame_decisions_directory,frame_locations,frame_sizes,header)
 
         # get the sparse matrixes
         #
@@ -88,23 +97,15 @@ def main():
 
         # generate a tuple of framesizes
         #
-        framesize_fib = (int(framesizes[0]),int(framesizes[0]))
+        framesize_fib = (int(frame_sizes[0]),int(frame_sizes[0]))
 
         # generate a heatmap of labels
         #
         heatmap = ann_tools.heatmap(sparse_matrixes,framesize_fib)
-
+        
+        # generate the regions
+        #
         regions = eval_tools.generate_region_decisions(heatmap,framesize_fib[0])
-        
-        print(regions)
-        
-        #if generate_histogram == 1:
-        #    eval_tools.plot_histogram(labels,histogram_output)
-
-    # print the error rate and mean confidence %
-    #
-    # print("Accuracy rate = ",model.score(mydata,labels))
-    # print("Mean confidence %",eval_tools.mean_confidence(model,mydata))
     
 
 if __name__ == "__main__":

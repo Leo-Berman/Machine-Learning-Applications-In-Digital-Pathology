45cd29efaf735f13b69a27c4d330185d7e3d4dae
Leo Berman <LeoGrantBerman@gmail.com>
2024-09-30 22:15:09 -0400

    Fixing typo

--- nedc_mladp/src/ref/main.py
+++ nedc_mladp/src/ref/main.py
@@ -30,7 +30,7 @@ def main():
 
 
         annotation_tool = nedc_dpath_ann_tools.AnnDpath()
-        annotation_tool.load(annotation_list[image_index])
+        annotation_tool.load(annotations_list[image_index])
         
         master_dictionary[images_processed] = {
             'header':annotation_tool.get_header(),
